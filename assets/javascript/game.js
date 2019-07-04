d3fd7d16358540b376332600c012b14a5785647b
Lauren <lwfriel@gmail.com>
2019-07-03 19:56:36 -0500

    generated random number and gems function

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,34 +1,45 @@
-
-var targetNumber = 53;
-
-$("#total-score").text(targetNumber);
-// computer to generate random number 19-120 and store in total crystal number
-// generate a random number between 1-12
-// store number into variable for each gem 
-// on click for gems 
-// number + total score
-// total score = > total number  
-// if = total crystal number; you win 
-// if >; you lose 
-
-var counter = 0;
-var numberOptions = [10, 5, 3, 7];
-
-$(".crystal-image").on("click", function () {
-
-    var crystalValue = ($(this).attr("data-crystalvalue"));
-    crystalValue = parseInt(crystalValue);
-
-    counter += crystalValue;
-
-    alert("Your new score is: " + counter);
+$(document).ready(function () {
 
 
-    if (counter === targetNumber) {
-        alert("You win!")
-    }
 
-    else if (counter >= targetNumber) {
-        alert("You lose!!")
-    }
-});
+  // computer to generate random number 19-120 and store in total crystal number
+  // generate a random number between 1-12 for each gem 
+  // store number into variable for each gem 
+  // on click for gems 
+  // number + total score
+  // total score = > total number  
+  // if = total crystal number; you win 
+  // if >; you lose 
+
+  var randomNumber = 0;
+  var gem1 = 0;
+  var gem2 = 0;
+  var gem3 = 0;
+  var gem4 = 0;
+  var totalscore = 0;
+  var wins = 0;
+  var losses = 0;
+
+  function getRandomInteger(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+  }
+
+  function start() {
+    randomNumber = getRandomInteger(19, 120);
+    gem1 = getRandomInteger(1, 12);
+    gem2 = getRandomInteger(1, 12);
+    gem3 = getRandomInteger(1, 12);
+    gem4 = getRandomInteger(1, 12);
+  }
+  start();
+  console.log (randomNumber);
+  console.log (gem1);
+  console.log (gem2);
+  console.log (gem3);
+  console.log (gem4);
+
+
+
+})
